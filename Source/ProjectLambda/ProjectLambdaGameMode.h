efcc5dabc7419a5ae3eff4300af64ed4722f8c8a
berkealgul <berkealgul@gmail.com>
2021-07-25 01:26:45 +0300

    initial

---
+++ Source/ProjectLambda/ProjectLambdaGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ProjectLambdaGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AProjectLambdaGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AProjectLambdaGameMode();
+};
+
+
+
---
+++ Source/ProjectLambda/ProjectLambdaHUD.h
@@ -0,0 +1,25 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once 
+
+#include "CoreMinimal.h"
+#include "GameFramework/HUD.h"
+#include "ProjectLambdaHUD.generated.h"
+
+UCLASS()
+class AProjectLambdaHUD : public AHUD
+{
+	GENERATED_BODY()
+
+public:
+	AProjectLambdaHUD();
+
+	/** Primary draw call for the HUD */
+	virtual void DrawHUD() override;
+
+private:
+	/** Crosshair asset pointer */
+	class UTexture2D* CrosshairTex;
+
+};
+
